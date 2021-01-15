8d4cf788b00860626e913127f538bf4db176f360
Melinda Muller Account <mmullerx4@gmail.com>
2021-01-15 15:10:14 -0500

    fixed coding

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let cDate = new Date();
+let cYear = cDate.getFullYear();
+document.getElementById("newYear").textContent = cYear;
+
+document.getElementById("lastDate").textContent = "Last Updated: " + document.lastModified;
