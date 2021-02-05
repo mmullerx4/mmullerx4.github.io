e410a373bc838da2004cebc9ab55dcd888439387
Melinda Muller Account <mmullerx4@gmail.com>
2021-02-05 11:15:10 -0500

    update

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+var date=new Date();
+var weekList=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthList=["January", "Febrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("date").innerHTML=weekList[date.getDay()]  + ", " + date.getDate() +   " " + monthList[date.getMonth()] +  " " + date.getFullYear();
