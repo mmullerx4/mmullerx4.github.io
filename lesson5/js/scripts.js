8e25d619c5123d7d7728584f950a77343873c9bd
Melinda Muller Account <mmullerx4@gmail.com>
2021-02-18 21:55:05 -0500

    update

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -7,3 +7,19 @@ var date=new Date();
 var weekList=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var monthList=["January", "Febrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.getElementById("date").innerHTML=weekList[date.getDay()]  + ", " + date.getDate() +   " " + monthList[date.getMonth()] +  " " + date.getFullYear();
+
+
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDate();
+console.log(dayNumber);
+
+const element = document.getElementById("message");
+
+if (dayNumber == 6) {
+    element.classList.add("showme");
+}
+    else {
+        element.classList.add("hideme");
+    }
