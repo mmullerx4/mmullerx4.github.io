4af188369ca00a4ea53acc757fc9838a9ed29c13
mmullerx4 <mmullerx4@gmail.com>
2021-03-17 19:43:30 -0400

    update

---
+++ lesson11/js/scripts.js
@@ -0,0 +1,34 @@
+function toggleMenu() {
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+var date=new Date();
+var weekList=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthList=["January", "Febrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("date").innerHTML=weekList[date.getDay()]  + ", " + date.getDate() +   " " + monthList[date.getMonth()] +  " " + date.getFullYear();
+
+
+        const today = new Date();
+        console.log(today);
+
+        const dayNumber = today.getDate();
+        console.log(dayNumber);
+
+        const element = document.getElementById("message");
+
+        if (dayNumber == 6) {
+            element.classList.add("showme");
+        } else {
+            element.classList.add("hideme");
+        }
+    
+        
+        WebFont.load({
+            google: {
+                families: ['Roboto']
+            }
+        });
+
+        const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+    
