e6d3f38d2ea438f2e385f6c34c4074f145d615fc
mmullerx4 <mmullerx4@gmail.com>
2021-03-20 08:52:24 -0400

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,11 +1,11 @@
-const apiURL = "https//api.openweathermap.org/data/2.5/weather?id=5604473&APPID=5fd8fde588cf618ac179af2d49411b66&units=imperial";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=5fd8fde588cf618ac179af2d49411b66";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
     const temperature = document.querySelector('#temperature');
-    currentTemp.textContent = jsObject.main.temp;
+    
     document.getElementById('current-temp').textContent = jsObject.main.temp;
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
