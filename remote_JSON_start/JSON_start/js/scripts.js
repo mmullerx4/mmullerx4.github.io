8887588cf7d444fcafbc6333c2941b0c50319fab
mmullerx4 <mmullerx4@gmail.com>
2021-04-03 07:13:04 -0400

    update

--- remote_JSON_start/JSON_start/js/scripts.js
+++ remote_JSON_start/JSON_start/js/scripts.js
@@ -1,14 +1,16 @@
 //ADD the key and change units to imperial
-const apiURL = ""
+const apiURL = "api.openweathermap.org/data/2.5/weather?id=5816861&appid=5fd8fde588cf618ac179af2d49411b66&units=imperial"
 
 //Go fetch it and then wait for a response.
+//weatherInfo can be any name you want.
 fetch(apiURL)
   .then((response) => response.json())
-  .then((changeme) => {
+  .then((weatherInfo) => {
     //Once it comes back, display it to the console.
-    console.log(changeme);
+    console.log(weatherInfo);
     
-    
+    //place will be the name & weatherInfo comes from the then statement
+    document.getElementById('place').innerHTML="weatherInfo",name;
 
  }); //end of "then" fat arrow function
 
