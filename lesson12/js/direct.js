32d0498fcdc7b4f1d31ac01c845bfba039ef5bd2
mmullerx4 <mmullerx4@gmail.com>
2021-04-07 21:40:55 -0400

    update

---
+++ lesson12/js/direct.js
@@ -0,0 +1,15 @@
+
+fetch("coc.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+
+        const directory = jsonObject['directory'];
+        for (let i = 0; i < directory.length; i++) {
+
+
+        let 
+
+        }
+    }
