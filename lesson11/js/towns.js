d9fa76699110e254309c2d0b07ea2d50c67d15e0
mmullerx4 <mmullerx4@gmail.com>
2021-04-08 10:45:33 -0400

    update

--- lesson11/js/towns.js
+++ lesson11/js/towns.js
@@ -1,4 +1,5 @@
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+//const requestUR = 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL = './js/towndata.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -6,23 +7,60 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
 
-    
-    const towndata = jsonObject['towndata'];
+    const allTowns = jsonObject['towns'];
+    const towndata = allTowns.filter(item => item.name === "Fish Haven" || item.name === "Preston" ||
+    item.name === "Soda Springs");
+    console.log(towndata);
     for (let i = 0; i < towndata.length; i++) {
 
 
      
-
-      //add paragraphs <p> for field items and an image <img>.
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
+//create elements (creates field to add information)
+      let homecard = document.createElement('section');
+      let towncontent = document.createElement('div');
+      let tTitle = document.createElement('div');
+      let tData= document.createElement('div');
+      let homeh2 = document.createElement('h2');
+      let homeh3 = document.createElement('h3');
+      let p1 = document.createElement('p');
+      let p2 = document.createElement('p');
+      let p3 = document.createElement('p');
       let image = document.createElement('img');
-      h2.textContent = towndata[i].name + ' ' + towndata[i].lastname;
+     
+      //add json data to elements (assigning the data)
+      homeh2.textContent = towndata[i].name;
+      homeh3.textContent = towndata[i].motto;
+      p1.textContent = "Year Founded: " + towndata[i].yearFounded;
+      p2.textContent = "Current Population: " + towndata[i].currentPopulation;
+      p3.textContent = "Average Rainfall: " + towndata[i].averageRainfall;
 
-      card.appendChild(h2);
-      card.appendChild(image);
-      document.querySelector('div.cards').appendChild(card);
+      //Attributes
+      homecard.setAttribute('class', 'homecard');
+      towncontent.setAttribute('class', 'towncontent');
+      tTitle.setAttribute('class', 't-title');
+      tData.setAttribute('class', 't-data');
+      homeh2.setAttribute('class', 'homeh2');
+      homeh3.setAttribute('class', 'homeh3');
+      p1.setAttribute('class', 'p1');
+      p2.setAttribute('class', 'p2');
+      p3.setAttribute('class', 'p3');
+      image.setAttribute('src', 'images/' + towndata[i].photo);
+      image.setAttribute('class', 'homeimg');
 
-      image.setAttribute('src', towndata[i].imageurl);
+      //organize elements to create card
+      tTitle.appendChild(homeh2);
+      tTitle.appendChild(homeh3);
+      tData.appendChild(p1);
+      tData.appendChild(p2);
+      tData.appendChild(p3);
+      towncontent.appendChild(tTitle);
+      towncontent.appendChild(tData);
+      homecard.appendChild(towncontent);
+      homecard.appendChild(image);
+      
+
+      document.querySelector('div.hometowns').appendChild(homecard);
+
+
     }
   });
