c7c2814afe41e1ad555a7a9478f7dafb25b44e33
mmullerx4 <mmullerx4@gmail.com>
2021-04-08 22:48:01 -0400

    update

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -47,5 +47,75 @@ fetch(apiURL)
     }
 
   });
+/*
+  apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=5fd8fde588cf618ac179af2d49411b66&units=imperial";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const forecast = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
+    console.log(forecast);
+    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for (let i = 0; i < forecast.length; i++) {
+      let date = new Date(forecast[i].dt_txt);
+
+      //creating the elements
+      let div = document.createElement('div');
+      let h5 = document.createElement('h5');
+      let img = document.createElement('img');
+      let p = document.createElement('p');
+      //filling content of elements
+      const imagesrc = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png'; // note the concatenation
+      p.textContent = forecast[i].main.temp;
+      img.setAttribute('src', imagesrc);
+      img.setAttribute('alt', forecast[i].weather[0].description);
+
+      h5.textContent = weekDays[date.getDay()];
+      //organize elements in proper parent/child order under div
+      div.appendChild(h5);
+      div.appendChild(img);
+      div.appendChild(p);
+      //put the forecast into the section (parent of div)
+      document.getElementById('forecast').appendChild(div);
+    }
+
+  });
+
+  apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=5fd8fde588cf618ac179af2d49411b66&units=imperial";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const forecast = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
+    console.log(forecast);
+    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for (let i = 0; i < forecast.length; i++) {
+      let date = new Date(forecast[i].dt_txt);
+
+      //creating the elements
+      let div = document.createElement('div');
+      let h5 = document.createElement('h5');
+      let img = document.createElement('img');
+      let p = document.createElement('p');
+      //filling content of elements
+      const imagesrc = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png'; // note the concatenation
+      p.textContent = forecast[i].main.temp;
+      img.setAttribute('src', imagesrc);
+      img.setAttribute('alt', forecast[i].weather[0].description);
+
+      h5.textContent = weekDays[date.getDay()];
+      //organize elements in proper parent/child order under div
+      div.appendChild(h5);
+      div.appendChild(img);
+      div.appendChild(p);
+      //put the forecast into the section (parent of div)
+      document.getElementById('forecast').appendChild(div);
+    }
+
+  }); */
+
+  
 
   
