d2464ce8345be3b2417e71553e1762e91f61af69
mmullerx4 <mmullerx4@gmail.com>
2021-04-11 17:55:23 -0400

    update

--- lesson12/js/direct.js
+++ lesson12/js/direct.js
@@ -11,37 +11,36 @@ fetch("coc.json")
                 //create elememts (creates field to add information)
 
                 let bdirectory = document.createElement('div');
-                let ul = document.createElement('ul');
-                let li1 = document.createElement('li');
-                let li2 = document.createElement('li');
-                let li3 = document.createElement('li');
-                let li4 = document.createElement('li');
-                let li5 = document.createElement('li');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
+                let p5 = document.createElement('p');
 
                 //add json to elements (assigning the data)
-                li1.textContent = directory[i].name;
-                li2.textContent = directory[i].organization;
-                li3.textContent = "Address1" + directory[i].address1;
-                li4.textContent = "Address2" + directory[i].address2;
-                li4.textContent = "Phone" + directory[i].phone;
+                p1.textContent = directory[i].name;
+                p2.textContent = directory[i].organization;
+                p3.textContent = "Address1" + directory[i].address1;
+                p4.textContent = "Address2" + directory[i].address2;
+                p5.textContent = "Phone" + directory[i].phone;
 
                 //attributes
                 bdirectory.setAttribute('class', 'bdirectory');
-                ul.setAttribute('class', 'ulist');
-                li1.setAttribute('class', 'li1');
-                li2.setAttribute('class', 'li2');
-                li3.setAttribute('class', 'li3');
-                li4.setAttribute('class', 'li4');
-                li5.setAttribute('class', 'li5');
+                p1.setAttribute('class', 'p1');
+                p2.setAttribute('class', 'p2');
+                p3.setAttribute('class', 'p3');
+                p4.setAttribute('class', 'p4');
+                p5.setAttribute('class', 'p5');
 
                 //organize elements to create card
                 ulist.appendChild(bdirectory);
-                ulist.appendChild(li1);
-                ulist.appendChild(li2);
-                ulist.appendChild(li3);
-                ulist.appendChild(li4);
-                ulist.appendChild(li5);
+                ulist.appendChild(p1);
+                ulist.appendChild(p2);
+                ulist.appendChild(p3);
+                ulist.appendChild(p4);
+                ulist.appendChild(p5);
 
+                document.querySelector('div.bdirectory')
             
             }
-        }
+        });
